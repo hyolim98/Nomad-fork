62441f1dfff9cd7762997252d26f701b5e6ec0db
hyolim98 <hyolim.korea@gmail.com>
2024-09-27 08:11:02 +0000

    edit scripts

--- compile.sh
+++ compile.sh
@@ -1,46 +1,46 @@
 root_dir=$PWD
 
 mkdir -p ${root_dir}/src/tmp/
-rm -rf ${root_dir}/src/tmp/*
+#rm -rf ${root_dir}/src/tmp/*
 
-rm src/linux-5.15.19/.config
-cp ${root_dir}/src/kernel_config/5.15.config src/linux-5.15.19/.config
+#rm src/linux-5.15.19/.config
+#cp ${root_dir}/src/kernel_config/5.15.config src/linux-5.15.19/.config
 
 ulimit -n 2048
 # compile memtis
-echo compiling memtis...
-echo deleting compiled code...
-sudo rm -rf src/linux-5.15.19/*
-echo restoring source code...
-git restore src/linux-5.15.19/
+#echo compiling memtis...
+#echo deleting compiled code...
+#sudo rm -rf src/linux-5.15.19/*
+#echo restoring source code...
+#git restore src/linux-5.15.19/
 
-rm -f src/linux-*_amd64.*
-echo start compiling...
-docker run -v .:/root/code --rm docklf/ubuntu20-kerncomp:aec-v0.2 bash /root/code/src/docker_commands/docker_comp5.15.19.sh
+#rm -f src/linux-*_amd64.*
+#echo start compiling...
+#docker run -v .:/root/code --rm docklf/ubuntu20-kerncomp:aec-v0.2 bash /root/code/src/docker_commands/docker_comp5.15.19.sh
 
-cp ${root_dir}/src/*.deb ${root_dir}/src/tmp/
+#cp ${root_dir}/src/*.deb ${root_dir}/src/tmp/
 
-for i in ${root_dir}/src/tmp/*.deb
-do
-newname=`echo $i | sed 's+[0-9]\{1,3\}_amd64+memtis_amd64+g'`
+#for i in ${root_dir}/src/tmp/*.deb
+#do
+#newname=`echo $i | sed 's+[0-9]\{1,3\}_amd64+memtis_amd64+g'`
 
-if [ "$newname" != $i ];then
-mv $i $newname
-fi
+#if [ "$newname" != $i ];then
+#mv $i $newname
+#fi
 
-done
+#done
 
-rm src/linux-5.13-rc6/.config
-cp ${root_dir}/src/kernel_config/tpp.config src/linux-5.13-rc6/.config
+#rm src/linux-5.13-rc6/.config
+#cp ${root_dir}/src/kernel_config/tpp.config src/linux-5.13-rc6/.config
 # compile tpp
 echo compiling tpp...
-echo deleting patched code...
-sudo rm -rf src/linux-5.13-rc6/*
-echo restoring source code...
-git restore src/linux-5.13-rc6/
-echo applying patches...
-patch -p1 < src/implementation_patches/tpp.patch
-rm -f src/linux-*_amd64.*
+#echo deleting patched code...
+#sudo rm -rf src/linux-5.13-rc6/*
+#echo restoring source code...
+#git restore src/linux-5.13-rc6/
+#echo applying patches...
+#patch -p1 < src/implementation_patches/tpp.patch
+#rm -f src/linux-*_amd64.*
 
 echo start compiling...
 docker run -v .:/root/code --rm docklf/ubuntu20-kerncomp:aec-v0.2 bash /root/code/src/docker_commands/docker_comp5.13.sh
@@ -58,33 +58,33 @@ fi
 
 done
 
-rm src/linux-5.13-rc6/.config
-cp ${root_dir}/src/kernel_config/nomad.config src/linux-5.13-rc6/.config
+#rm src/linux-5.13-rc6/.config
+#cp ${root_dir}/src/kernel_config/nomad.config src/linux-5.13-rc6/.config
 # compile nomad
-echo compiling nomad...
-echo deleting patched code...
-sudo rm -rf src/linux-5.13-rc6/*
-echo restoring source code...
-git restore src/linux-5.13-rc6/
-echo applying patches...
-patch -p1 < src/implementation_patches/nomad.patch
-rm -f src/linux-*_amd64.*
+#echo compiling nomad...
+#echo deleting patched code...
+#sudo rm -rf src/linux-5.13-rc6/*
+#echo restoring source code...
+#git restore src/linux-5.13-rc6/
+#echo applying patches...
+#patch -p1 < src/implementation_patches/nomad.patch
+#rm -f src/linux-*_amd64.*
 
-echo start compiling...
-docker run -v .:/root/code --rm docklf/ubuntu20-kerncomp:aec-v0.2 bash /root/code/src/docker_commands/docker_comp5.13.sh
+#echo start compiling...
+#docker run -v .:/root/code --rm docklf/ubuntu20-kerncomp:aec-v0.2 bash /root/code/src/docker_commands/docker_comp5.13.sh
 
-cp ${root_dir}/src/*.deb ${root_dir}/src/tmp/
+#cp ${root_dir}/src/*.deb ${root_dir}/src/tmp/
 
-for i in ${root_dir}/src/tmp/*.deb
-do
+#for i in ${root_dir}/src/tmp/*.deb
+#do
 
-newname=`echo $i | sed 's+[0-9]\{1,3\}_amd64+nomad_amd64+g'`
+#newname=`echo $i | sed 's+[0-9]\{1,3\}_amd64+nomad_amd64+g'`
 
-if [ "$newname" != $i ];then
-mv $i $newname
-fi
+#if [ "$newname" != $i ];then
+#mv $i $newname
+#fi
 
-done
+#done
 
 echo compiling userspace program...
 
@@ -94,7 +94,7 @@ echo generate microbenchmark access pattern file...
 docker run -v .:/root/code --rm docklf/ubuntu20-kerncomp:aec-v0.2 bash /root/code/src/testing_scripts/microbenchmark/generate_ycsb.sh
 
 mkdir -p src/tmp/output
-rm -rf src/tmp/output/*
+#rm -rf src/tmp/output/*
 
 mv src/testing_scripts/microbenchmark/tmp/*.bin src/tmp/output/
 mv src/tmp/*.deb src/tmp/output/
